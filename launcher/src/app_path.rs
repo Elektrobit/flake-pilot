03aa94391f00c7aeda6fbaf2a021d1ce520db04e
Marcus Schäfer <marcus.schaefer@gmail.com>
2022-07-12 21:01:10 +0200

    Support program runtime config file
    Options to control how podman starts the container can now be
    provided as /etc/pilot/program_name.yaml like the following
    example shows:
    
    runtime:
      --storage-opt: size=10G
      --rm:
    
    If no runtime configuration exists the following defaults apply
    
    * Container resources will be automatically deleted after the call.
    * Interactive sessions will be allowed

--- launcher/src/app_path.rs
+++ launcher/src/app_path.rs
@@ -1,6 +1,11 @@
+extern crate yaml_rust;
+
 use std::env;
 use which::which;
 use std::path::Path;
+use std::fs;
+use yaml_rust::Yaml;
+use yaml_rust::YamlLoader;
 
 pub fn program_abs_path() -> String {
     /*!
@@ -22,4 +27,23 @@ pub fn basename(program_path: &String) -> String {
         Path::new(program_path).file_name().unwrap().to_str().unwrap()
     );
     program_name
+}
+
+pub fn program_config_file(program_basename: &String) -> String {
+    /*!
+    Provide expected config file path for the given program_basename
+    !*/
+    let config_file = &format!("/etc/pilot/{}.yaml", program_basename);
+    config_file.to_string()
+}
+
+pub fn program_config(program_basename: &String) -> Vec<Yaml> {
+    /*!
+    Read container runtime configuration for given program
+    !*/
+    let config_file = program_config_file(program_basename);
+    let yaml_content = fs::read_to_string(&config_file)
+        .expect(&format!("Failed to read program config {}", config_file));
+    let yaml = YamlLoader::load_from_str(&yaml_content).unwrap();
+    yaml
 }
--- launcher/src/tests.rs
+++ launcher/src/tests.rs
@@ -2,12 +2,19 @@ use which::which;
 use std::env;
 use crate::app_path::program_abs_path;
 use crate::app_path::basename;
+use crate::app_path::program_config_file;
 use crate::container_link::container_name;
 
 #[test]
 fn test_program_abs_path() {
     let program_path = program_abs_path();
     assert!(program_path.starts_with("/"));
+}
+
+#[test]
+fn test_program_config_file() {
+    let config_file = program_config_file(&format!("app"));
+    assert_eq!("/etc/pilot/app.yaml", config_file);
 }
 
 #[test]
