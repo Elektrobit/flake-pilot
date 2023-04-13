5e04fca5e6b081af89bd3a7845dc98f8beb1fff8
Marcin Katulski <extern.marcin.katulski@elektrobit.com>
2023-04-13 15:51:02 +0200

    Fixes after review

--- firecracker-pilot/firecracker-service/service/src/main.rs
+++ firecracker-pilot/firecracker-service/service/src/main.rs
@@ -1,3 +1,26 @@
+//
+// Copyright (c) 2022 Elektrobit Automotive GmbH
+//
+// This file is part of flake-pilot
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 extern crate daemonize;
 
 use std::fs::File;
@@ -10,6 +33,11 @@ use daemonize::Daemonize;
 mod app;
 use crate::app::handle_incoming_connections;
 
+/**
+   Module defines default values like file names for output etc.
+ */
+mod defaults;
+use crate::defaults::*;
 
 #[macro_use]
 extern crate log;
@@ -22,8 +50,8 @@ fn main() {
         firecracker-service is a service meant to run in background, to provide unix-domain socket
         that will be used to communicate with it. 
     */    
-    let stdout = File::create("/tmp/firecracker-service.out").unwrap();
-    let stderr = File::create("/tmp/firecracker-service.err").unwrap();
+    let stdout = File::create(FC_SERVICE_OUT).unwrap();
+    let stderr = File::create(FC_SERVICE_ERR).unwrap();
     
     setup_logger();
 
