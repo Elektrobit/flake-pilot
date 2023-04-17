25cdac04522c1e30977a7d791464697931df8b69
Marcus Schäfer <marcus.schaefer@gmail.com>
2023-04-17 15:58:40 +0200

    Make sure container layer get pulled in
    If using additional container layers, the launcher should
    make sure they get pulled if not yet done.

--- podman-pilot/src/podman.rs
+++ podman-pilot/src/podman.rs
@@ -552,6 +552,9 @@ pub fn mount_container(
         call.arg("--user").arg(user);
     }
     if as_image {
+        if ! container_image_exists(&container_name, &user) {
+            pull(&container_name, &user);
+        }
         call.arg("podman").arg("image").arg("mount").arg(&container_name);
     } else {
         call.arg("podman").arg("mount").arg(&container_name);
@@ -783,6 +786,61 @@ pub fn container_running(cid: &String, user: &String) -> bool {
         }
     }
     running_status
+}
+
+pub fn container_image_exists(name: &str, user: &str) -> bool {
+    /*!
+    Check if container image is present in local registry
+    !*/
+    let mut exists_status = false;
+    let mut exists = Command::new("sudo");
+    if ! user.is_empty() {
+        exists.arg("--user").arg(&user);
+    }
+    exists.arg("podman")
+        .arg("image").arg("exists").arg(name);
+    debug(&format!("{:?}", exists.get_args()));
+    match exists.status() {
+        Ok(status) => {
+            if status.code().unwrap() == 0 {
+                exists_status = true
+            }
+        },
+        Err(_) => { }
+    }
+    exists_status
+}
+
+pub fn pull(uri: &str, user: &str) {
+    /*!
+    Call podman pull and prune with the provided uri
+    !*/
+    let mut pull = Command::new("sudo");
+    if ! user.is_empty() {
+        pull.arg("--user").arg(&user);
+    }
+    pull.arg("podman").arg("pull").arg(uri);
+    debug(&format!("{:?}", pull.get_args()));
+    match pull.status() {
+        Ok(status) => {
+            if ! status.success() {
+                panic!("Failed, error message(s) reported");
+            } else {
+                let mut prune = Command::new("sudo");
+                if ! user.is_empty() {
+                    prune.arg("--user").arg(&user);
+                }
+                prune.arg("podman").arg("image").arg("prune").arg("--force");
+                match prune.status() {
+                    Ok(_) => { },
+                    Err(_) => { }
+                }
+            }
+        }
+        Err(status) => {
+            panic!("Failed to call podman pull: {}", status)
+        }
+    }
 }
 
 pub fn update_removed_files(
