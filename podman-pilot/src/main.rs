ee873ade1265df1d3731c535d2ec3a4a8b98e67c
Michael Meyer <ichmed95@gmail.com>
2023-07-26 17:33:06 +0200

    Replaced all panics, unwraps and exits with errors

--- podman-pilot/src/main.rs
+++ podman-pilot/src/main.rs
@@ -26,26 +26,50 @@ extern crate log;
 
 #[cfg(test)]
 pub mod tests;
+pub mod error;
 
+use std::process::{ExitCode, Termination};
+
+use config::config;
 use env_logger::Env;
+use error::FlakeError;
 
 pub mod app_path;
 pub mod podman;
 pub mod defaults;
 pub mod config;
 
-fn main() {
+fn main() -> ExitCode {
     setup_logger();
+    // load config now so we can terminate early if the config is invalid
+    config();
+    // past here there should be no more panics
+
+    let result = run();
+
+    // TODO: implement cleanup function 
+    // cleanup()
+
+    match result {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(err) => {
+            error!("{err}");
+            err.report()
+        },
+    }
+}
+
+fn run() -> Result<(), FlakeError> {
 
     let program_path = app_path::program_abs_path();
     let program_name = app_path::basename(&program_path);
 
-    let container = podman::create(&program_name);
-    let cid = &container[0];
+    let container = podman::create(&program_name)?;
+    let cid = &container.0;
     podman::start(
         &program_name,
         cid
-    );
+    )
 }
 
 fn setup_logger() {
