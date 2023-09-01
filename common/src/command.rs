4f74458e419cebaa8cabebf52a7a13e9da628058
Michael Meyer <ichmed95@gmail.com>
2023-09-01 11:52:19 +0200

    Clippy

--- common/src/command.rs
+++ common/src/command.rs
@@ -1,6 +1,49 @@
-use std::{fmt::{Display, Write}, process::Command, ffi::OsStr};
+use std::{fmt::{Display, Write}, process::{Command, Output, CommandArgs}, ffi::OsStr};
 
 use thiserror::Error;
+
+pub trait CommandExtTrait {
+    /// Execute this command and return:
+    /// 
+    /// 1. An IO Error if the command could not be run
+    /// 2. An Execution Error if the Command was not successfull
+    /// 3. The [Output] of the Command if the command was executed successfully
+    /// 
+    /// Attaches all args to the resulting error
+    /// 
+    /// If a termination with a non 0 exit status is considered succesful
+    /// this method should not be used.
+    fn perform(&mut self) -> Result<std::process::Output, CommandError>;
+}
+
+impl CommandExtTrait for Command {
+    fn perform(&mut self) -> Result<std::process::Output, CommandError> {
+        handle_output(self.output(), self.get_args())
+    }
+}
+
+pub fn handle_output(maybe_output: Result<Output, std::io::Error>, args: CommandArgs) -> Result<std::process::Output, CommandError> {
+    let out = maybe_output.map_err(ProcessError::IO);
+
+    let error: ProcessError = match out {
+        Ok(output) => {
+            if output.status.success() {
+                return Ok(output);
+            } else {
+                output.into()
+            }
+        }
+        Err(err) => err,
+    };
+
+    Err(CommandError {
+        base: error,
+        args: args
+            .flat_map(OsStr::to_str)
+            .map(ToOwned::to_owned)
+            .collect(),
+    })
+}
 
 #[derive(Debug, Error)]
 pub enum ProcessError {
@@ -53,38 +96,5 @@ impl From<std::process::Output> for CommandError {
 impl From<ProcessError> for CommandError {
     fn from(value: ProcessError) -> Self {
         Self { base: value, args: Default::default() }
-    }
-}
-
-pub trait CommandExtTrait {
-    /// Execute this command and return:
-    ///
-    /// 1. An IO Error if the command could not be run
-    /// 2. An Execution Error if the Command was not successfull
-    /// 3. The [Output] of the Command if the command was executed successfully
-    ///
-    /// Attaches all args to the resulting error
-    ///
-    /// If a termination with a non 0 exit status is considered succesful
-    /// this method should not be used.
-    fn perform(&mut self) -> Result<std::process::Output, CommandError>;
-}
-
-impl CommandExtTrait for Command {
-    fn perform(&mut self) -> Result<std::process::Output, CommandError> {
-        let out = self.output().map_err(ProcessError::IO);
-
-        let error: ProcessError = match out {
-            Ok(output) => {
-                if output.status.success() {
-                    return Ok(output);
-                } else {
-                    output.into()
-                }
-            }
-            Err(err) => err,
-        };
-
-        Err(CommandError { base: error, args: self.get_args().flat_map(OsStr::to_str).map(ToOwned::to_owned).collect() })
     }
 }
