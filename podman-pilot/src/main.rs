d711ef79ae0d0986e05bbb3fe3917ef409aad803
Bo Maryniuk <bo@maryniuk.net>
2023-09-04 15:47:41 +0200

    Flatten and refactor main

--- podman-pilot/src/main.rs
+++ podman-pilot/src/main.rs
@@ -2,51 +2,46 @@
 extern crate log;
 
 pub mod error;
-#[cfg(test)]
-pub mod tests;
 
-use std::process::{ExitCode, Termination};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+    process::{ExitCode, Termination},
+};
 
 use config::config;
 use env_logger::Env;
 use error::FlakeError;
 
-pub mod app_path;
 pub mod config;
 pub mod defaults;
 pub mod podman;
 
+fn setup_logger() {
+    let env = Env::default().filter_or("MY_LOG_LEVEL", "trace").write_style_or("MY_LOG_STYLE", "always");
+    env_logger::init_from_env(env);
+}
+
+fn run() -> Result<(), FlakeError> {
+    let program_name = Path::new(fs::canonicalize(PathBuf::from(env::args().next().unwrap())).unwrap().to_str().unwrap())
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    podman::start(&program_name, &podman::create(&program_name)?.0)
+}
+
 fn main() -> ExitCode {
     setup_logger();
-    // load config now so we can terminate early if the config is invalid
     config();
-    // past here there should be no more panics
 
-    let result = run();
-
-    // TODO: implement cleanup function
-    // cleanup()
-
-    match result {
-        Ok(()) => ExitCode::SUCCESS,
+    match run() {
+        Ok(_) => ExitCode::SUCCESS,
         Err(err) => {
             error!("{err}");
             err.report()
         }
     }
-}
-
-fn run() -> Result<(), FlakeError> {
-    let program_path = app_path::program_abs_path();
-    let program_name = app_path::basename(&program_path);
-
-    let container = podman::create(&program_name)?;
-    let cid = &container.0;
-    podman::start(&program_name, cid)
-}
-
-fn setup_logger() {
-    let env = Env::default().filter_or("MY_LOG_LEVEL", "trace").write_style_or("MY_LOG_STYLE", "always");
-
-    env_logger::init_from_env(env);
 }
