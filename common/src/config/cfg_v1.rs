de9c640a195bab25e90399459d9a764bc81cbd09
Bo Maryniuk <bo@maryniuk.net>
2023-09-15 00:15:05 +0200

    Add versioned config module scaffold

---
+++ common/src/config/cfg_v1.rs
@@ -0,0 +1,5 @@
+/// Configuration parser, v1.
+///
+/// It is the original version of the config,
+/// prior to 2.2.19 Flakes version.
+pub struct FlakeCfgV1 {}
---
+++ common/src/config/itf.rs
@@ -0,0 +1,16 @@
+/// FlakeConfig is an interface for all configuration possible
+/// across all suppirted versions of the config.
+pub struct FlakeConfig {}
+
+impl Default for FlakeConfig {
+    fn default() -> Self {
+        FlakeConfig {}
+    }
+}
+
+impl FlakeConfig {
+    /// Create an instance of a FlakeConfig
+    pub fn new() -> Self {
+        FlakeConfig {}
+    }
+}
