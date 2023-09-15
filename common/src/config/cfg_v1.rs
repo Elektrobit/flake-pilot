c593031dddda3a608d19b4c91ad1f86e1d2b88f0
Bo Maryniuk <bo@maryniuk.net>
2023-09-15 20:48:57 +0200

    Implement config parser, v1 (scaffold)

--- common/src/config/cfg_v1.rs
+++ common/src/config/cfg_v1.rs
@@ -1,5 +1,25 @@
+use std::path::PathBuf;
+
+use crate::config::{cfgparse::FlakeCfgVersionParser, itf::FlakeConfig};
+
 /// Configuration parser, v1.
 ///
 /// It is the original version of the config,
 /// prior to 2.2.19 Flakes version.
-pub struct FlakeCfgV1 {}
+///
+pub struct FlakeCfgV1 {
+    path: PathBuf,
+}
+
+impl FlakeCfgV1 {
+    pub fn new(path: PathBuf) -> Self {
+        FlakeCfgV1 { path }
+    }
+}
+
+impl FlakeCfgVersionParser for FlakeCfgV1 {
+    fn parse(&self) -> super::itf::FlakeConfig {
+        println!("Looking for {}", self.path.to_str().unwrap());
+        FlakeConfig::default()
+    }
+}
