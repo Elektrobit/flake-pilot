13acc6e65484c2392d768c1cd13755360b8006e1
Bo Maryniuk <bo@maryniuk.net>
2023-09-15 20:53:29 +0200

    Update example usage

--- common/src/main.rs
+++ common/src/main.rs
@@ -2,8 +2,13 @@ mod config;
 use crate::config::cfgparse::FlakeCfgParser;
 use std::path::PathBuf;
 
+/// Example multiversion config usage
 pub fn main() {
     println!("{:-<80}", "");
-    let x = FlakeCfgParser::new(PathBuf::from("../../robot_tests/data/config-v1/firecracker.yaml"));
-    let c = x.parse();
+
+    let parser: FlakeCfgParser = FlakeCfgParser::new(PathBuf::from("../../robot_tests/data/config-v1/firecracker.yaml"));
+    let cfg: Option<config::itf::FlakeConfig> = parser.parse();
+    if let Some(cfg) = cfg {
+        println!("{:?}", cfg);
+    }
 }
