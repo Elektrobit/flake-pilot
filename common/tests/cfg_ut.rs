afe522783200e166fc175de1704c20ee62685734
Bo Maryniuk <bo@maryniuk.net>
2023-09-17 18:55:11 +0200

    Add Unit Tests for configuration parsers

---
+++ common/tests/cfg_ut.rs
@@ -0,0 +1,34 @@
+#[cfg(test)]
+mod tests {
+    use core::panic;
+    use flakes::config::{cfgparse::FlakeCfgParser, itf::FlakeConfig};
+    use std::path::PathBuf;
+
+    /// Prepare FlakeCfgParser
+    fn setup(cfg_path: String) -> Option<FlakeConfig> {
+        FlakeCfgParser::new(PathBuf::from(cfg_path)).parse()
+    }
+
+    /// Test Bundle
+    fn tb<T>(cfg_path: String, probe: T) -> ()
+    where
+        T: Fn(Option<FlakeConfig>) -> () + panic::UnwindSafe,
+    {
+        probe(setup(cfg_path));
+    }
+
+    /// Test Firecracker configuration v1 overall parse
+    #[test]
+    fn test_cfg_v1_fc_overall_parse() {
+        tb("data/cfg-v1/firecracker.yaml".to_string(), |cfg| {
+            assert!(cfg.is_some(), "FlakeConfig v1 for firecracker should not be None");
+        });
+    }
+
+    #[test]
+    fn test_cfg_v1_pdm_overall_parse() {
+        tb("data/cfg-v1/podman.yaml".to_string(), |cfg| {
+            assert!(cfg.is_none(), "FlakeConfig v1 for podman should not be None");
+        });
+    }
+}
