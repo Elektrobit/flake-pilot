a12589944f3a0c66a39ad103e959abe18009af2c
Bo Maryniuk <bo@maryniuk.net>
2023-09-25 19:40:50 +0200

    Add UT for the v2 runtime name

--- common/tests/cfg_ut_v2.rs
+++ common/tests/cfg_ut_v2.rs
@@ -13,4 +13,12 @@ mod cfg_v2_ut {
             assert!(cfg.unwrap().version() == 2, "Version should be 2");
         });
     }
+
+    /// Test v2 overall parse
+    #[test]
+    fn test_cfg_v2_runtime_name() {
+        ut_rt::tb("cfg-v2/all.yaml".to_string(), |cfg| {
+            assert!(cfg.unwrap().runtime().image_name() == "darth vader", "The name should be defined");
+        });
+    }
 }
