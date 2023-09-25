748f311ba6627137db8f1e2d5a98957bced29c2a
Bo Maryniuk <bo@maryniuk.net>
2023-09-25 19:46:18 +0200

    Add UT for v2 on path map should have properties

--- common/tests/cfg_ut_v2.rs
+++ common/tests/cfg_ut_v2.rs
@@ -3,6 +3,8 @@ mod ut_rt;
 /// Unit tests for v2 config
 #[cfg(test)]
 mod cfg_v2_ut {
+    use std::path::PathBuf;
+
     use super::ut_rt;
 
     /// Test v2 overall parse
@@ -28,6 +30,17 @@ mod cfg_v2_ut {
         ut_rt::tb("cfg-v2/all.yaml".to_string(), |cfg| {
             assert!(!cfg.clone().unwrap().runtime().paths().is_empty(), "Path map should not be empty");
             assert!(cfg.unwrap().runtime().paths().len() == 4, "Path map should have four elements");
+        });
+    }
+
+    /// Test v2 path map has properties
+    #[test]
+    fn test_cfg_v2_path_map_has_props() {
+        ut_rt::tb("cfg-v2/all.yaml".to_string(), |cfg| {
+            assert!(
+                cfg.unwrap().runtime().paths().get(&PathBuf::from("/usr/bin/banana")).is_some(),
+                "Banana should have some properties!"
+            );
         });
     }
 }
