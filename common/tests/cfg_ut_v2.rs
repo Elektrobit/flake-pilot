fae5f90a0f414351ede28474da80994e705e4cfc
Bo Maryniuk <bo@maryniuk.net>
2023-09-27 00:47:08 +0200

    Add UT test_cfg_v2_engine_params_rtp_vcpu

--- common/tests/cfg_ut_v2.rs
+++ common/tests/cfg_ut_v2.rs
@@ -196,4 +196,14 @@ mod cfg_v2_ut {
             );
         });
     }
+
+    #[test]
+    fn test_cfg_v2_engine_params_rtp_vcpu() {
+        ut_rt::tb("cfg-v2/all.yaml".to_string(), |cfg| {
+            assert!(
+                FirecrackerRuntimeParams::from(cfg.unwrap().engine().params().unwrap()).vcpu_count().unwrap() == 2,
+                "Runtime params should have some virtual CPUs"
+            );
+        });
+    }
 }
