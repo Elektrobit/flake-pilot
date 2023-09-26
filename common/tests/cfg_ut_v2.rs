fa05fedf3211682af0191be42e57c91c7208dd98
Bo Maryniuk <bo@maryniuk.net>
2023-09-27 00:30:15 +0200

    Add UT for test_cfg_v2_engine_parsed

--- common/tests/cfg_ut_v2.rs
+++ common/tests/cfg_ut_v2.rs
@@ -111,4 +111,11 @@ mod cfg_v2_ut {
             );
         });
     }
+
+    #[test]
+    fn test_cfg_v2_engine_parsed() {
+        ut_rt::tb("cfg-v2/all.yaml".to_string(), |cfg| {
+            assert!(cfg.unwrap().engine().pilot() == "RD2D".to_string(), "Pilot should always have RD2D!");
+        });
+    }
 }
