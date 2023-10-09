6cd2985fdc4c85e1b8cbac5c3196381f5b41b493
Michael Meyer <ichmed95@gmail.com>
2023-10-09 09:38:04 +0200

    Streamlines Builder setup

--- flake-ctl/flake-ctl-build-rpmbuild/src/main.rs
+++ flake-ctl/flake-ctl-build-rpmbuild/src/main.rs
@@ -1,28 +1,184 @@
-use anyhow::Result;
-use clap::Parser;
-use rpmbuild::Tooling;
-use flake_ctl_build::{self, FlakeBuilder, Path, PathBuf};
+use anyhow::{Context, Result};
+use clap::{builder::OsStr, Args, ValueEnum};
+use flakes::config::{self, itf::FlakeConfig};
+use fs_extra::{copy_items, dir::CopyOptions};
+use tempfile::tempdir_in;
 
-mod rpmbuild;
+use flake_ctl_build::{export_flake, FlakeBuilder, PackageOptions, PathBuf};
 
 fn main() -> Result<()> {
-    let args = Args::parse();
-    let template = args.template.unwrap_or_else(|| Path::new("/usr/share/flakes/package/debbuild").to_owned());
-    rpmbuild::Builder { template_dir: &template, edit: !args.no_edit, tooling: args.tooling }.run(&args.builder_args)?;
-    Ok(())
+    flake_ctl_build::run::<RPMBuilder>()
 }
 
-#[derive(Parser)]
-struct Args {
-    #[command(flatten)]
-    builder_args: flake_ctl_build::BuilderArgs,
+use std::{
+    fs::{self, copy, create_dir_all, remove_dir_all, OpenOptions},
+    io::Write,
+    path::Path,
+    process::Command,
+};
 
-    #[arg(long)]
-    template: Option<PathBuf>,
+#[derive(Args)]
+pub struct RPMBuilder {
+    /// Location of .spec template and pilot specific data
+    #[arg(long, default_value = "/usr/share/flakes/package/rpmbuild")]
+    template: PathBuf,
 
+    /// skip spec editing
     #[arg(long)]
     no_edit: bool,
 
+    /// What toolchain to use for building
     #[arg(long, value_enum, default_value = Tooling::RPMBuild)]
     tooling: Tooling,
+}
+
+#[derive(Debug, Clone, ValueEnum, Copy)]
+#[value(rename_all = "lower")]
+pub enum Tooling {
+    RPMBuild,
+    DebBuild,
+}
+
+impl From<Tooling> for OsStr {
+    fn from(value: Tooling) -> Self {
+        match value {
+            Tooling::RPMBuild => "rpmbuild".into(),
+            Tooling::DebBuild => "debbuild".into(),
+        }
+    }
+}
+
+impl FlakeBuilder for RPMBuilder {
+    fn setup(&self, location: &Path) -> Result<()> {
+        self.infrastructure(location, create_dir_all)
+    }
+
+    fn create_bundle(&self, options: &PackageOptions, config: &FlakeConfig, location: &Path) -> Result<()> {
+        let PackageOptions { name, version, .. } = options;
+        let temp_dir = tempdir_in(location).context("Failed to create bundling dir")?;
+        let bundling_dir = temp_dir.path().join(format!("{name}-{version}"));
+
+        create_dir_all(&bundling_dir)?;
+
+        self.copy_includes(config, &bundling_dir).context("Failed to copy includes to bundling dir")?;
+        self.copy_configs(name, &bundling_dir).context("Failed to copy configs to bundling dir")?;
+        export_flake(name, config.engine().pilot(), &bundling_dir).context("Failed to export flake image(s)")?;
+        self.compress_bundle(temp_dir.path(), name, version).context("Failed to compress bundle")?;
+        copy(bundling_dir.with_extension("tar.gz"), location.join("SOURCES").join(name).with_extension("tar.gz"))
+            .context("Failed to move bundle to build dir")?;
+
+        self.create_spec(&location.join("SPECS"), options, config).context("Failed to create spec")?;
+
+        Ok(())
+    }
+
+    fn build(&self, options: &PackageOptions, target: Option<&Path>, location: &Path) -> Result<()> {
+        Command::new(OsStr::from(self.tooling))
+            .arg("-ba")
+            .arg("--define")
+            .arg(format!("_topdir {}", location.to_string_lossy()))
+            .arg(location.join("SPECS").join(&options.name).with_extension("spec"))
+            .status()?;
+
+        let package_dir = match self.tooling {
+            Tooling::RPMBuild => "RPMS",
+            Tooling::DebBuild => "DEBS",
+        };
+        copy_items(&[location.join(package_dir)], target.unwrap_or_else(|| Path::new(".")), &CopyOptions::default())?;
+        Ok(())
+    }
+
+    fn cleanup(&self, location: &Path) -> Result<()> {
+        self.infrastructure(location, remove_dir_all)
+    }
+}
+
+impl RPMBuilder {
+    fn infrastructure<F, P>(&self, location: P, f: F) -> Result<()>
+    where
+        F: FnMut(PathBuf) -> Result<(), std::io::Error>,
+        P: AsRef<Path>,
+    {
+        ["BUILD", "SOURCES", "SPECS"]
+            .into_iter()
+            .chain(match self.tooling {
+                Tooling::RPMBuild => ["RPMS", "SRPMS"],
+                Tooling::DebBuild => ["DEBS", "SDEBS"],
+            })
+            .map(|x| location.as_ref().join(x))
+            .try_for_each(f)?;
+        Ok(())
+    }
+
+    fn copy_includes(&self, config: &FlakeConfig, bundling_dir: &Path) -> Result<()> {
+        let bundles = config.static_data().get_bundles().into_iter().flatten();
+        for tar in bundles {
+            copy(tar, bundling_dir.join(tar))?;
+        }
+        Ok(())
+    }
+    fn copy_configs(&self, name: &str, bundling_dir: &Path) -> Result<()> {
+        let config_path = Path::new("/usr/share/flakes").join(name);
+        let configs = [config_path.with_extension("yaml"), config_path.with_extension("d")];
+
+        let fake_flake_dir = bundling_dir.join("usr/share/flakes");
+        create_dir_all(&fake_flake_dir)?;
+        copy_items(&configs, &fake_flake_dir, &CopyOptions::new())?;
+        Ok(())
+    }
+
+    fn compress_bundle(&self, bundling_dir: &Path, name: &str, version: &str) -> Result<()> {
+        Command::new("tar")
+            .arg("-czvf")
+            .arg(bundling_dir.join(format!("{name}-{version}")).with_extension("tar.gz"))
+            .arg("-C")
+            .arg(bundling_dir)
+            .arg(".")
+            .status()?;
+        Ok(())
+    }
+
+    fn create_spec(&self, path: &Path, options: &PackageOptions, config: &FlakeConfig) -> Result<()> {
+        let spec_path = path.join(&options.name).with_extension("spec");
+        let mut spec = OpenOptions::new().write(true).create_new(true).open(&spec_path)?;
+        let content = self.construct_spec(config, options)?;
+        spec.write_all(content.as_bytes())?;
+        spec.flush()?;
+
+        // TODO: Select default text editor
+        if !self.no_edit {
+            Command::new("vi").arg(&spec_path).status().context("Failed to open text editor")?;
+        }
+        Ok(())
+    }
+
+    fn construct_spec(&self, config: &FlakeConfig, options: &PackageOptions) -> Result<String> {
+        let mut template =
+            fs::read_to_string(self.template.join("template").with_extension("spec")).context("Failed to read spec template")?;
+
+        // TODO: maybe use faster templating here
+        // TODO: Drop line with empty information completely
+        let data = self.template.join(config.engine().pilot());
+        let requires = fs::read_to_string(&data).context(format!("Failed to load pilot specific data, {data:?}"))?;
+
+        let vals = [
+            ("%{_flake_name}", options.name.as_str()),
+            ("%{_flake_version}", options.version.as_str()),
+            ("%{_flake_desc}", options.description.as_str()),
+            ("%{_flake_url}", options.url.as_str()),
+            ("%{_flake_license}", options.license.as_str()),
+            ("%{_flake_maintainer}", options.maintainer.as_str()),
+            ("%{_flake_pilot}", config.engine().pilot()),
+            ("%{_flake_requires}", &requires),
+            ("%{_flake_dir}", &config::FLAKE_DIR.to_string_lossy()),
+        ];
+
+        for (placeholder, value) in vals {
+            template = template.replace(placeholder, value);
+        }
+
+        // TODO: multiple links
+
+        Ok(template)
+    }
 }
--- flake-ctl/flake-ctl-build/src/lib.rs
+++ flake-ctl/flake-ctl-build/src/lib.rs
@@ -1,11 +1,10 @@
-use std::{fs::remove_dir_all, io::stdin, env::var, process::Command};
 pub use std::path::{Path, PathBuf};
+use std::{env::var, fs::remove_dir_all, io::stdin, process::Command};
 
-use anyhow::{Result, anyhow, Context};
-use clap::Args;
-use flakes::config::{itf::FlakeConfig, self};
+use anyhow::{anyhow, Context, Result};
+use clap::{Args, FromArgMatches, Parser};
+use flakes::config::{self, itf::FlakeConfig};
 use tempfile::tempdir;
-
 
 pub trait FlakeBuilder {
     /// Create the infrastructure to build the flake package in the given directory
@@ -20,23 +19,23 @@ pub trait FlakeBuilder {
     fn build(&self, options: &PackageOptions, target: Option<&Path>, location: &Path) -> Result<()>;
 
     /// Remove all the temporary infrastructure created in [setup]
-    /// 
+    ///
     /// This may be run before _and_ after each build
-    /// 
+    ///
     /// Only modify the contents of `location`, if the directory itself needs to be deleted this will be handled by [cleanup_default_directory]
     fn cleanup(&self, location: &Path) -> Result<()>;
 
     /// What directory to use when no build location is given, this defaults to a new tmp directory
-    /// 
+    ///
     /// Override this for your builder if there is another sensible default
-    /// 
+    ///
     /// Keep in mind that you may also have to adjust [cleanup_default_directory]
     fn get_default_build_directory(&self) -> Result<PathBuf> {
         Ok(tempdir()?.into_path())
     }
 
     /// Cleanup the directory structure of the default directory, this is only called when no build location was given
-    /// 
+    ///
     /// Default behaviour is to just delete the directory (assumed to be a tmp directory created by [get_default_build_directory]),
     /// override this function if this is undesired
     fn cleanup_default_directory(&self, dir: &Path) -> Result<()> {
@@ -47,31 +46,28 @@ pub trait FlakeBuilder {
     ///
     /// If `target` is given store the resulting package there, otherwise put it into the current working directory
     /// If `location` is given the package is build there, otherwise it is constructed in a tmp directory
-    /// 
+    ///
     /// [build] is only run if `build` is `true`
-    /// 
+    ///
     /// [cleanup] is not run if `keep` is `true`
     fn execute(
-        &self, options: &PackageOptions, target: Option<impl AsRef<Path>>, location: Option<impl AsRef<Path>>, build: bool, keep: bool,
+        &self, options: &PackageOptions, target: Option<impl AsRef<Path>>, location: Option<impl AsRef<Path>>, build: bool,
+        keep: bool,
     ) -> Result<()> {
         let cleanup_default = location.is_none();
-        
+
         let location = match location.as_ref() {
             Some(location) => location.as_ref().to_owned(),
             None => self.get_default_build_directory()?,
         };
-        
+
         // Clean the location if it is reused, ignore errors here
         self.cleanup(&location).ok();
         self.setup(&location)?;
         let config = config::load_from_target(Path::new(&options.name))?;
         self.create_bundle(options, &config, &location)?;
 
-        let result = if build {
-            self.build(options, target.as_ref().map(AsRef::as_ref), &location)
-        } else {
-            Ok(())
-        };
+        let result = if build { self.build(options, target.as_ref().map(AsRef::as_ref), &location) } else { Ok(()) };
 
         if !keep {
             self.cleanup(&location)?;
@@ -86,21 +82,20 @@ pub trait FlakeBuilder {
         let options = args.determine_options()?;
         self.execute(&options, args.target.as_ref(), args.location.as_ref(), !args.dry_run, args.keep)
     }
-
 }
 
 #[derive(Debug)]
-pub struct PackageOptions{
+pub struct PackageOptions {
     pub name: String,
     pub description: String,
     pub version: String,
     pub url: String,
     pub maintainer: String,
-    pub license: String
+    pub license: String,
 }
 
 #[derive(Debug, Default, Args, Clone)]
-pub struct PackageOptionsBuilder{
+pub struct PackageOptionsBuilder {
     #[arg(long)]
     pub name: Option<String>,
     #[arg(long)]
@@ -112,18 +107,18 @@ pub struct PackageOptionsBuilder{
     #[arg(long)]
     pub maintainer: Option<String>,
     #[arg(long)]
-    pub license: Option<String>
+    pub license: Option<String>,
 }
 
 impl PackageOptionsBuilder {
     pub fn build(self) -> Result<PackageOptions> {
         Ok(PackageOptions {
-            name: self.name.ok_or_else(||anyhow!("Missing package name"))?,
-            description: self.description.ok_or_else(||anyhow!("Missing package description"))?,
-            version: self.version.ok_or_else(||anyhow!("Missing package version"))?,
-            url: self.url.ok_or_else(||anyhow!("Missing package url"))?,
-            maintainer: self.maintainer.ok_or_else(||anyhow!("Missing package maintainer"))?,
-            license: self.license.ok_or_else(||anyhow!("Missing package license"))?,
+            name: self.name.ok_or_else(|| anyhow!("Missing package name"))?,
+            description: self.description.ok_or_else(|| anyhow!("Missing package description"))?,
+            version: self.version.ok_or_else(|| anyhow!("Missing package version"))?,
+            url: self.url.ok_or_else(|| anyhow!("Missing package url"))?,
+            maintainer: self.maintainer.ok_or_else(|| anyhow!("Missing package maintainer"))?,
+            license: self.license.ok_or_else(|| anyhow!("Missing package license"))?,
         })
     }
 }
@@ -184,10 +179,9 @@ pub struct BuilderArgs {
 }
 
 impl BuilderArgs {
-
     pub fn determine_options(&self) -> Result<PackageOptions> {
         let mut options = self.options.clone();
-        
+
         // Read from env where not given
         options.name = options.name.or_else(|| var("PKG_FLAKE_NAME").ok());
         options.description = options.description.or_else(|| var("PKG_FLAKE_DESCRIPTION").ok());
@@ -195,7 +189,7 @@ impl BuilderArgs {
         options.url = options.url.or_else(|| var("PKG_FLAKE_URL").ok());
         options.maintainer = options.maintainer.or_else(|| var("PKG_FLAKE_MAINTAINER").ok());
         options.license = options.license.or_else(|| var("PKG_FLAKE_LICENSE").ok());
-    
+
         if !self.ci {
             options.name = options.name.or_else(|| user_input("Name").ok());
             options.description = options.description.or_else(|| user_input("Description").ok());
@@ -204,21 +198,33 @@ impl BuilderArgs {
             options.maintainer = options.maintainer.or_else(|| user_input("Maintainer").ok());
             options.license = options.license.or_else(|| user_input("License").ok());
         }
-    
+
         options.build().context("Missing packaging option")
     }
-    
-    }
-    
-    fn user_input(name: &str) -> Result<String> {
-        let mut buf = String::new();
-        println!("{name}: ");
-        stdin().read_line(&mut buf)?;
-        Ok(buf.trim_end().to_owned())
-    }
+}
 
+fn user_input(name: &str) -> Result<String> {
+    let mut buf = String::new();
+    println!("{name}: ");
+    stdin().read_line(&mut buf)?;
+    Ok(buf.trim_end().to_owned())
+}
 
-    pub fn export_flake(name: &str, pilot: &str, bundling_dir: &Path) -> Result<()> {
-        Command::new("flake-ctl").arg(pilot).arg("export").arg(name).arg(bundling_dir.join(name)).status()?;
-        Ok(())
-    }
+pub fn export_flake(name: &str, pilot: &str, bundling_dir: &Path) -> Result<()> {
+    Command::new("flake-ctl").arg(pilot).arg("export").arg(name).arg(bundling_dir.join(name)).status()?;
+    Ok(())
+}
+
+#[derive(Parser)]
+struct FullArgs<T: FromArgMatches + Args + FlakeBuilder> {
+    #[command(flatten)]
+    args: BuilderArgs,
+
+    #[command(flatten)]
+    builder: T,
+}
+
+pub fn run<T: FromArgMatches + Args + FlakeBuilder>() -> Result<()> {
+    let command = FullArgs::<T>::parse();
+    command.builder.run(&command.args)
+}
