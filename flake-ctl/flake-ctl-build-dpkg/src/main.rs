9a4911ef5038d2288c05830d5a2342e0532b309a
Michael Meyer <ichmed95@gmail.com>
2023-12-08 16:41:19 +0100

    Moves from dpkg-deb to dpkg-buildpackage

--- flake-ctl/flake-ctl-build-dpkg/src/main.rs
+++ flake-ctl/flake-ctl-build-dpkg/src/main.rs
@@ -2,13 +2,15 @@ use std::{
     fs::{self, create_dir_all, remove_dir_all, OpenOptions},
     io::Write,
     path::{Path, PathBuf},
-    process::Command,
+    process::Command, env::{set_current_dir, current_dir},
 };
 
 use anyhow::{Context, Ok, Result};
 use clap::Args;
 use flake_ctl_build::{copy_configs, export_flake, FlakeBuilder, PackageOptions, BuilderArgs};
 use flakes::{config::itf::FlakeConfig, paths::RootedPath};
+use fs_extra::{file::write_all, copy_items, dir::{CopyOptions, self}};
+use tempfile::tempdir;
 
 fn main() -> Result<()> {
     flake_ctl_build::run::<DPKGBuilder>()
@@ -27,7 +29,7 @@ struct DPKGBuilder {
 
 impl FlakeBuilder for DPKGBuilder {
     fn description(&self) -> &str {
-        "Package flakes with dpkg-deb"
+        "Package flakes with dpkg-buildpackage"
     }
 
     fn setup(&self, location: &Path) -> Result<()> {
@@ -38,10 +40,15 @@ impl FlakeBuilder for DPKGBuilder {
         &self, flake_name: &RootedPath, args: &BuilderArgs, options: &PackageOptions, config: &FlakeConfig, location: &Path,
     ) -> Result<()> {
         let edit = !self.no_edit && !args.ci;
-        self.control_file(options, location, config.engine().pilot(), edit).context("Failed to create control file")?;
-        self.rules_file(location).context("Failed to create rules file")?;
-        self.install_script(location, config).context("Failed to create install script")?;
-        self.uninstall_script(location, config).context("Failed to create uninstall script")?;
+        
+        self.control(options, location, config.engine().pilot(), edit).context("Failed to create control file")?;
+        self.rules(location).context("Failed to create rules file")?;
+        self.postinst(location, config).context("Failed to create install script")?;
+        self.prerm(location, config).context("Failed to create uninstall script")?;
+        self.compat(location)?;
+        self.install(location, &flake_name.file_name().unwrap().to_string_lossy(), config)?;
+        self.changelog(location, options)?;
+
         let export_path = &location.join(flakes::config::FLAKE_DIR.strip_prefix("/").unwrap());
         create_dir_all(export_path)?;
         create_dir_all(location.join("tmp"))?;
@@ -52,14 +59,20 @@ impl FlakeBuilder for DPKGBuilder {
         Ok(())
     }
 
-    fn build(&self, options: &PackageOptions, target: Option<&Path>, location: &Path) -> Result<()> {
-        Command::new("dpkg-deb")
-            .arg("--root-owner-group")
-            .arg("--nocheck")
-            .arg("-b")
-            .arg(location)
-            .arg(target.unwrap_or_else(|| Path::new(".")).join(&options.name).with_extension("deb"))
-            .status()?;
+    fn build(&self, _options: &PackageOptions, target: Option<&Path>, location: &Path) -> Result<()> {
+        // Move all content of staging directory to a temporary folder, cd there, build, and then copy the result to the output folder
+        // This is needed because of a quirk in dpkg-buildpackage where the package is always build in `..`
+        let tmp = tempdir()?;
+        copy_items(&[location], tmp.path(), &CopyOptions::new())?;
+        let cwd = current_dir()?;
+        let tmp_build = tmp.path().join(location.file_name().unwrap());
+        set_current_dir(&tmp_build)?;
+
+        Command::new("dpkg-buildpackage").args(["-us", "-uc"]).status()?;
+
+        set_current_dir(cwd)?;
+        remove_dir_all(tmp_build)?;
+        dir::copy(tmp.path(), target.unwrap_or(Path::new(".")), &CopyOptions::new().content_only(true).overwrite(true))?;
         Ok(())
     }
 
@@ -74,47 +87,59 @@ impl DPKGBuilder {
         F: FnMut(PathBuf) -> Result<(), std::io::Error>,
         P: AsRef<Path>,
     {
-        ["tmp", "usr", "DEBIAN"].into_iter().map(|x| location.as_ref().join(x)).try_for_each(f)?;
+        ["tmp", "usr", "debian"].into_iter().map(|x| location.as_ref().join(x)).try_for_each(f)?;
         Ok(())
     }
 
-    fn control_file(&self, options: &PackageOptions, location: &Path, pilot: &str, edit: bool) -> Result<()> {
+    fn control(&self, options: &PackageOptions, location: &Path, pilot: &str, edit: bool) -> Result<()> {
         let depends = fs::read_to_string(self.template.join(pilot))?;
 
-        let mut cfile = OpenOptions::new().create(true).write(true).open(location.join("DEBIAN").join("control"))?;
+        let mut cfile = OpenOptions::new().truncate(true).create(true).write(true).open(location.join("debian").join("control"))?;
+
 
         [
+            // Do not remove as_str() or the type inference will break
+            ("Source", options.name.as_str()),
             ("Section", "other"),
             ("Priority", "optional"),
             ("Maintainer", &format!("\"{}\" <{}>", options.maintainer_name, options.maintainer_email)),
             ("Homepage", &options.url),
-            ("Package", &options.name),
+            ("Standard-Version", &options.version),
+        ]
+        .into_iter()
+        .try_for_each(|(name, value)| cfile.write_all(format!("{name}: {value}\n").as_bytes()))?;
+
+        cfile.write_all("\n\n".as_bytes())?;
+
+        [
+            // Do not remove as_str() or the type inference will break
+            ("Package", options.name.as_str()),
             ("Architecture", "all"),
-            ("Multi-Arch", "foreign"),
             ("Depends", &depends),
+            ("Multi-Arch", "foreign"),
             ("Description", &options.description),
-            ("Version", &options.version),
             ("Package-Type", "deb"),
             ("Rules-Requires-Root", "binary-targets"),
         ]
         .into_iter()
         .try_for_each(|(name, value)| cfile.write_all(format!("{name}: {value}\n").as_bytes()))?;
 
+
         // TODO: Select default text editor
         if edit {
-            Command::new("vi").arg(location.join("DEBIAN").join("control")).status().context("Failed to open text editor")?;
+            Command::new("vi").arg(location.join("debian").join("control")).status().context("Failed to open text editor")?;
         }
         Ok(())
     }
 
-    fn rules_file(&self, location: &Path) -> Result<()> {
-        OpenOptions::new().create(true).write(true).open(location.join("DEBIAN/rules"))?.write_all("%:\n\tdh $@".as_bytes())?;
+    fn rules(&self, location: &Path) -> Result<()> {
+        OpenOptions::new().create(true).write(true).open(location.join("debian/rules"))?.write_all("%:\n\tdh $@".as_bytes())?;
         Ok(())
     }
 
-    fn install_script(&self, location: &Path, conf: &FlakeConfig) -> Result<()> {
+    fn postinst(&self, location: &Path, conf: &FlakeConfig) -> Result<()> {
         let pilot = conf.engine().pilot();
-        let mut script = OpenOptions::new().create(true).write(true).open(location.join("DEBIAN").join("postinst"))?;
+        let mut script = OpenOptions::new().create(true).write(true).open(location.join("debian").join("postinst"))?;
 
         let archive = Path::new("/tmp").join(conf.runtime().image_name());
         let archive = archive.to_string_lossy();
@@ -129,14 +154,46 @@ impl DPKGBuilder {
         Ok(())
     }
 
-    fn uninstall_script(&self, location: &Path, conf: &FlakeConfig) -> Result<()> {
-        let mut script = OpenOptions::new().create(true).write(true).open(location.join("DEBIAN").join("prerm"))?;
+    fn prerm(&self, location: &Path, conf: &FlakeConfig) -> Result<()> {
+        let mut script = OpenOptions::new().create(true).write(true).open(location.join("debian").join("prerm"))?;
         script.write_all(format!("podman rmi {}\n", conf.runtime().image_name()).as_bytes())?;
         conf.runtime()
             .paths()
             .keys()
             .map(|x| x.to_string_lossy())
             .try_for_each(|path| script.write_all(format!("rm {path}\n").as_bytes()))?;
+        Ok(())
+    }
+
+    fn compat(&self, location: &Path) -> Result<()> {
+        write_all(location.join("debian/compat"), "10\n")?;
+        Ok(())
+    }
+    
+    fn changelog(&self, location: &Path, options: &PackageOptions) -> Result<()> {
+        let PackageOptions { name, version, maintainer_name, maintainer_email, .. } = options;
+        // TODO: make settable in options
+        let status = "UNRELEASED";
+        let urgency = "medium";
+        // TODO: Maybe offer feature extract from other package
+        let content = "* This is an automatically packaged flake, see chengelog of original for details";
+        
+        // TODO: calc this
+        let time = "Wed, 06 Dec 2023 13:56:16 +0100";
+
+        let content = format!("{name} ({version}) {status}; urgency={urgency}\n\n{content}\n\n-- {maintainer_name} <{maintainer_email}>  {time}");
+        write_all(location.join("debian/changelog"), &content)?;
+        Ok(())
+    }
+
+    fn install(&self, location: &Path, flake_name: &str, config: &FlakeConfig) -> Result<()> {
+        let yaml = Path::new("usr/share/flakes").join(flake_name).with_extension("yaml");
+        let yaml = yaml.to_string_lossy();
+        let d = Path::new("usr/share/flakes").join(flake_name).with_extension("d");
+        let d = d.to_string_lossy();
+        let image = Path::new("tmp").join(config.runtime().image_name());
+        let image = image.to_string_lossy();
+        write_all(location.join("debian/install"), &format!("{yaml} /usr/share/flakes\n{d} /usr/share/flakes\n{image} /tmp\n"))?;
         Ok(())
     }
 }
